1e39ee8f1eb64f986f736cf4b0c8eb11b47357af
somatic3000-dev <somatic3000@googlemail.com>
2026-05-12 20:00:20 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div style={{
+      background: "#111",
+      color: "#fff",
+      minHeight: "100vh",
+      padding: "40px",
+      fontFamily: "Arial"
+    }}>
+      <h1>APEX PE APP</h1>
+      <p>Deployment funktioniert.</p>
+    </div>
+  );
+}
